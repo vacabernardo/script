3ecd6e6b4be5907deff5b1c25ba2339233786f65
vacabernardo <bernardo@manubrio.mx>
2023-05-10 18:20:25 -0600

    1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<script type = "text/javascript">
-window.onload = function() {
-    // similar behavior as clicking on a link
-    window.location.href = "http://stackoverflow.com";
-}
-      </script>
+<script>window.onload = function() {
+        // similar behavior as clicking on a link
+        window.location.href = "http://stackoverflow.com";
+    }</script>
